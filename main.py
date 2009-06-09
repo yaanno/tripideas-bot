eb7868d960be01ccdd389491b357482a05e1fa89
yaanno <yaanno@gmail.com>
2009-06-09 08:31:47 +0200

    more kayak api enhancements

--- main.py
+++ main.py
@@ -46,50 +46,42 @@ class KayakApi(webapp.RequestHandler):
   def get(self):
     
     kayak_session = kayak.get_session()
-    self.response.out.write('<div style="float:left"><label>Get session:</label><br><textarea cols="50" rows="10">%s</textarea></div>' % kayak_session.content)
-    
     
     session_id = re.search('<sid>(.*?)</sid>', kayak_session.content)
     session_id = session_id.group(1)
     
-    self.response.out.write('<div style="float:left"><label>Session ID:</label><br><textarea cols="50" rows="10">%s</textarea></div>' % session_id)
+    kayak.session_id = session_id
+    kayak.headers = { 'Cookie' : kayak_session.headers['set-cookie'] }
     
-    cluster = re.search('cluster=(.*?);',kayak_session.headers['set-cookie'])
-    cluster = cluster.group(1)
+    kayak_search = kayak.post_search()
     
-    self.response.out.write('<div style="float:left"><label>Cluster ID:</label><br><textarea cols="50" rows="10">%s</textarea></div>' % cluster)
-    
-    
-    kayak_headers = {'cluster': cluster}
-    
-    kayak_search = kayak.post_search(session_id, kayak_headers)
-    self.response.out.write('<div style="float:left"><label>Get search:</label><br><textarea cols="50" rows="10">%s</textarea></div>' % kayak_search.content)
+    #self.response.out.write('<div style="float:left"><label>Get search:</label><br><textarea cols="50" rows="10">%s</textarea></div>' % kayak_search.content)
     
     
     search_id = re.search('<searchid>(.*?)</searchid>', kayak_search.content)
     search_id = search_id.group(1)
-    self.response.out.write('<div style="float:left"><label>Search ID:</label><br><textarea cols="50" rows="10">%s</textarea></div>' % search_id)
     
-    kayak_results = kayak.get_results(session_id, search_id, kayak_headers)
+    #self.response.out.write('<div style="float:left"><label>Search ID:</label><br><textarea cols="50" rows="10">%s</textarea></div>' % search_id)
+    
+    kayak.search_id = search_id
+    kayak_results = kayak.get_results()
+    
     self.response.out.write('<div style="float:left"><label>Search results:</label><br><textarea cols="50" rows="10">%s</textarea></div>' % kayak_results.content)
-    
-    
-    
-    
-    
-    
-    
-    '''
+
     result_set = []
  
     more_pending = re.search('<morepending>true</morepending>',kayak_results.content)
     
-    if more_pending.group(1) == 'true':
-      kayak_results = kayak.get_results(session_id,search_id, kayak_headers)
+    if more_pending.group(0) is not None:
+      more_pending = True
+    
+    if more_pending:
+      kayak.cycle_poll(result_set)
+      
+      kayak_results = kayak.get_results()
       result_set.append(kayak_results.content)
       
     self.response.out.write('<div style="float:left"><label>Search results:</label><br><textarea cols="50" rows="10">%s</textarea></div>' % result_set)
-    '''
 
 
 def main():
--- modules/kayak/api.py
+++ modules/kayak/api.py
@@ -2,6 +2,14 @@
 from google.appengine.api import urlfetch
 
 class Api(object):
+
+  headers = {}
+  deadline = 10
+  method = urlfetch.GET
+  follow_redirects = False
+  session_id = ''
+  search_id = ''
+
   def __init__(self, api_token, domain):
     self.api_token = api_token,
     self.domain = domain
@@ -11,7 +19,7 @@ class Api(object):
     result = urlfetch.fetch(url=url, method=urlfetch.GET, deadline=10, follow_redirects=False)
     return result
   
-  def post_search(self,sid,headers):
+  def post_search(self):
     basicmode = 'true'
     oneway = 'n'
     origin = "MAD"
@@ -26,16 +34,22 @@ class Api(object):
     action = "doFlights"
     apimode = "1"
     version = "1"
+    sid = self.session_id
     url = (self.domain + '/s/apisearch?basicmode=%s&oneway=%s&origin=%s&destination=%s&destcode=%s&depart_date=%s&depart_time=%s&return_date=%s&return_time=%s&travelers=%s&cabin=%s&action=%s&apimode=%s&_sid_=%s&version=%s' % (
       basicmode, oneway, origin, destination, destcode, depart_date, depart_time, return_date, return_time, travelers, cabin, action, apimode, sid, version ))
     
-    result = urlfetch.fetch(url=url, method=urlfetch.GET, headers=headers, deadline=10,follow_redirects=False)
+    result = urlfetch.fetch(
+      url=url, 
+      method=self.method, 
+      headers=self.headers, 
+      deadline=self.deadline,
+      follow_redirects=self.follow_redirects)
     return result
     
     
-  def get_results(self, sid, searchid, headers):
-    searchid = searchid
-    sid = sid
+  def get_results(self):
+    sid = self.session_id
+    searchid = self.search_id
     c = "10"
     version = "1"
     apimode  = "1"
@@ -43,8 +57,17 @@ class Api(object):
     sort = "price"
     direction = "down"
     url = (self.domain + '/s/apibasic/flight?searchid=%s&apimode=%s&_sid_=%s' % (searchid, apimode, sid))
-    result = urlfetch.fetch(url=url, method=urlfetch.GET, headers=headers, deadline=10,follow_redirects=False)
+    result = urlfetch.fetch(
+      url=url,
+      method=self.method, 
+      headers=self.headers, 
+      deadline=self.deadline,
+      follow_redirects=self.follow_redirects)
     return result
+
+  def cycle_poll(self, result_set):
+    pass
+
 
 class KayakError:
   pass
